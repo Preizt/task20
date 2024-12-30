55c5188bb63885ca450900c47f94e8561704cac3
Preizt <prieztly.tennysson@gmail.com>
2024-12-30 22:11:51 +0530

    first commit

---
+++ console.js
@@ -0,0 +1,198 @@
+//Yazar
+
+/*WAP to print prime numbers in a range*/
+
+
+//Fazil
+
+
+//WAP print the following pattern using nested loops:
+// 1
+// 22
+// 333
+// 4444
+// 55555
+
+
+// for(let i=1;i<=5;i++){
+//     let str="";
+
+//     for(let j=1;j<=i;j++)
+//     {
+//         str=str+i;
+//     }
+    
+    
+//     console.log(str);
+// }
+
+// Abhijith
+
+//Create a nested array and access all elements by using for in loop.
+
+
+//Archa
+
+//WAP to print the following pattern
+
+// 1 2 3 4 5 
+// 1 2 3 4 
+// 1 2 3 
+// 1 2
+// 1
+
+
+// for(let i=5;i>=1;i--){
+//     str=""
+//     for(let j=1;j<=i;j++)
+//     {
+//         str=str+j;
+//     }
+//     console.log(str);
+    
+// }
+
+//Akhila
+
+// Print an inverted right-angle triangle
+
+// *****
+// ****
+// ***
+// **
+// *
+
+
+// for(let i=5;i>=1;i--){
+//     str=""
+//     for(let j=1;j<=i;j++)
+//     {
+//         str=str+"*";
+//     }
+//     console.log(str);
+    
+// }
+
+
+
+//Sahad
+
+// Matrix Addition: Represent two 2D arrays and write a program to calculate their sum.
+
+
+// let arrOne = [[1,2,3,4,5],
+//              [6,7,8,9,10]]
+// let sum=0;
+
+
+// for(let i of arrOne)
+//     {
+//         // console.log(i);
+        
+//     for(let j of i){
+//         // console.log(j);
+        
+//         sum+=j
+//     }
+// }
+// console.log(sum);
+
+
+
+
+//Anandhu
+
+//Array Operations: Write a program to:
+// Add a new element to the end of the array.
+// Remove the first element of the array.
+// Print the updated array.
+
+// let fruits = [1,2,3,4,5,6,7,8,9,10];
+// let newArray=[];
+// for (let i of fruits){
+//  newArray.push(i)
+// }
+
+// newArray.push(11);
+//    newArray.shift();
+//    console.log(fruits);
+   
+//    console.log(newArray);
+
+
+//Chandana
+
+//Write a JavaScript program that prints the Fibonacci sequence up to the 20th term using a for loop.
+
+
+
+
+//Preizt
+
+//Sort the array and replace elements with * if the element is odd.
+
+
+// let arr = [8,7,6,5,4,3,2,1,23,4,56,7,10];
+// let newArr = []
+
+// arr.sort((a,b)=>a-b);
+
+// console.log(arr);
+
+
+// for(let i of arr){
+//     if(i%2!=0){
+//         i="*";
+//         newArr.push(i)
+//     }else{
+//         newArr.push(i)
+//     }
+// }
+// console.log(newArr);
+
+
+
+//Sreepriya 
+
+//Find the Frequency of Elements
+
+//Sreehari
+
+//Print the following pattern
+
+// # # # # # # 
+// # # # # # #
+// # # # # # #
+// # # # # # #
+// # # # # # #
+// # # # # # #
+
+
+// for(let i = 1 ;i<=6;i++){
+//     str="";
+//     for(let j=1;j<=6;j++){
+//         str=str+" # "
+//     }
+//     console.log(str);
+// }
+
+//Shereena
+
+// let j,i;
+
+// for(i = 1;i<=5;i++)
+// {
+//     str="";
+//     for( j=1;j<=5;j++)
+//         str=str+"*"
+//     if(i===1 || i===5){                   //Doubt
+//         console.log(str);
+//     }else{
+//         console.log(" ");
+        
+//     }
+    
+// }
+
+
+
